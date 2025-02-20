c15c7644253c584e7b4b88aa50b31668c6df8c3b
vomeeraq <den_karimov_06@mail.ru>
2025-02-20 16:45:15 +0500

    Update Intersection of Two Arrays II.java

--- Intersection of Two Arrays II.java
+++ Intersection of Two Arrays II.java
@@ -1,1 +1,24 @@
-
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+        
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        
+        for (int num : nums2) {
+            if (map.containsKey(num) && map.get(num) > 0) {
+                result.add(num);
+                map.put(num, map.get(num) - 1);
+            }
+        }
+        
+        int[] resArray = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            resArray[i] = result.get(i);
+        }
+        
+        return resArray;
+    }
+}
