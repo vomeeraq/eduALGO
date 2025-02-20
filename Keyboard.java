0fb226bed6e35f38b8b90de60b7ef582d16afe63
vomeeraq <den_karimov_06@mail.ru>
2025-02-20 16:45:04 +0500

    Update Keyboard.java

--- Keyboard.java
+++ Keyboard.java
@@ -1,1 +1,40 @@
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numKeys = scanner.nextInt();
+        int[] keyPressLimits = new int[numKeys];
+        
+        for (int i = 0; i < numKeys; i++) {
+            keyPressLimits[i] = scanner.nextInt();
+        }
+
+        int totalTaps = scanner.nextInt();
+        int[] tappedKeys = new int[totalTaps];
+        
+        for (int i = 0; i < totalTaps; i++) {
+            tappedKeys[i] = scanner.nextInt();
+        }
+
+        checkBrokenKeys(numKeys, keyPressLimits, totalTaps, tappedKeys);
+    }
+
+    public static void checkBrokenKeys(int numKeys, int[] keyPressLimits, int totalTaps, int[] tappedKeys) {
+        boolean[] isBroken = new boolean[numKeys];
+
+        for (int tap : tappedKeys) {
+            int keyIndex = tap - 1;
+            keyPressLimits[keyIndex]--;
+            
+            if (keyPressLimits[keyIndex] < 0) {
+                isBroken[keyIndex] = true;
+            }
+        }
+
+        for (int i = 0; i < numKeys; i++) {
+            System.out.println(isBroken[i] ? "YES" : "NO");
+        }
+    }
+}
