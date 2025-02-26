cb19a57bf65f9ab3f14d4869dcc11c1fa71bbbae
vomeeraq <den_karimov_06@mail.ru>
2025-02-26 21:00:24 +0500

    Update Постфиксная запись.java

--- Theme 3/Постфиксная запись.java
+++ Theme 3/Постфиксная запись.java
@@ -1,1 +1,34 @@
+import java.util.Scanner;
+import java.util.Stack;
 
+public class PostfixEvaluator {
+    public static int evaluatePostfix(String expression) {
+        Stack<Integer> stack = new Stack<>();
+        String[] tokens = expression.split(" ");
+
+        for (String token : tokens) {
+            if (token.equals("+") || token.equals("-") || token.equals("*")) {
+                int b = stack.pop();
+                int a = stack.pop();
+                if (token.equals("+")) {
+                    stack.push(a + b);
+                } else if (token.equals("-")) {
+                    stack.push(a - b);
+                } else if (token.equals("*")) {
+                    stack.push(a * b);
+                }
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.pop();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String expression = scanner.nextLine();
+        System.out.println(evaluatePostfix(expression));
+        scanner.close();
+    }
+}
