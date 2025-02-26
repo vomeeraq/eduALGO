fc9d78a0a9da5fcc306b4f360b94781dc6cd2384
vomeeraq <den_karimov_06@mail.ru>
2025-02-26 21:00:10 +0500

    Update Implement Queue using Stacks.java

--- Theme 3/Implement Queue using Stacks.java
+++ Theme 3/Implement Queue using Stacks.java
@@ -1,1 +1,38 @@
-
+class MyQueue {
+    private Stack<Integer> first;
+    private Stack<Integer> second;
+    public MyQueue() {
+        first = new Stack<>();
+        second = new Stack<>();
+    }
+    
+    public void push(int x) {
+        first.push(x);
+    }
+    
+    public int pop() {
+        while(!first.isEmpty()){
+        second.push(first.pop());
+        }
+        int removed = second.pop();
+        while(!second.isEmpty()){
+            first.push(second.pop());
+        }
+        return removed;
+    }
+    
+    public int peek() {
+        while(!first.isEmpty()){
+        second.push(first.pop());
+        }
+        int peek = second.peek();
+        while(!second.isEmpty()){
+            first.push(second.pop());
+        }
+        return peek;
+    }
+    
+    public boolean empty() {
+        return first.isEmpty();
+    }
+}
